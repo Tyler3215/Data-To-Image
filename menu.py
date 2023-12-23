1d6ea3bf1b1774e4e1d8b334835934abf62947ab
Tyler <tyler.zakrzewski@protonmail.com>
2023-12-23 11:59:39 +0100

    base menu

--- menu.py
+++ menu.py
@@ -1,58 +1,58 @@
 import main
 def ResolutionMenu(encoder):
     while True:
-        char = input(f"{'='*73}\n Do you want to change resoltuion of image? (default 640x480) 'Y' or 'N'\n")
-        if char == 'Y' or char == 'y': 
+        char = input(f"{'='*74}\n Do you want to change the resolution of the image?\n Enter 'Y' to change the resolution or 'N' to keep the default (640x480).\n{'='*74}\n")
+        if char.lower() == 'y': 
             encoder.SetResolution(input("Enter width: "), input("Enter height: "))
             break
 
-        elif char == 'N' or char == 'n': break
+        elif char.lower() == 'n': break
         else:
-            print("'Y' or 'N' only")
+            print("Please enter 'Y' or 'N' only.")
 
 def EncoderMenu(encoder):
     while True:
-        char = input(f"{'='*51}\n 'S' for encoding string 'F' for encoding txt file\n")
-        if char == 's' or char == 'S': 
+        char = input(f"{'='*60}\n Enter 'S' to encode a string or 'F' to encode a text file.\n{'='*60}\n")
+        if char.lower() == 's': 
             encoder.EncodeString(input("Enter string which you want to encode:\n"))
             break
 
-        elif char == 'd' or char == 'D':
+        elif char.lower() == 'f':
             encoder.EncodeFile(input("Enter path to file which you want to encode:\n"))
             break
 
         else:
-            print("'S' or 'F' only")
+            print("Please enter 'S' or 'F' only.")
 
 def DecoderMenu(decoder):
     while True:
-        char = input(f"{'='*57}\n 'S' for decoding to string 'F' for decoding to txt file\n")
-        if char == 's' or char == 'S': 
+        char = input(f"{'='*66}\n Enter 'S' to decode to a string or 'F' to decode to a text file.\n{'='*66}\n")
+        if char.lower() == 's': 
             string = decoder.DecodeToString()
             print(string)
             break
 
-        elif char == 'f' or char == 'F':
+        elif char.lower() == 'f':
             decoder.DecodeToFile(input("Enter path to txt file:\n"))
             break
 
         else:
-            print("'S' or 'F' only")
+            print("Please enter 'S' or 'F' only.")
 
 if __name__ == "__main__":
     while True:
-        char = input(f"{'='*15} MENU {'='*15}\n 'E' for encoder or 'D' for decoder\n")
-        if char == 'e' or char == 'E':
+        char = input(f"{'='*18} MENU {'='*18}\n Enter 'E' for encoder or 'D' for decoder\n{'='*42}\n")
+        if char.lower() == 'e':
             encoder = main.Encoder() 
             ResolutionMenu(encoder)
             EncoderMenu(encoder)
             break
             
-        elif char == 'd' or char == 'D':
+        elif char.lower() == 'd':
             decoder = main.Decoder()
             DecoderMenu(decoder)
             break
 
         else:
-            print("'E' or 'D' only")
+            print("Please enter 'E' or 'D' only.")
 
